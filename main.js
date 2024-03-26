ea218983429ab9f72b420e577d8b0f063a60f921
costa911 <costa.andrea911@gmail.com>
2024-03-26 18:18:54 +0100

    Updated main.js, grid cards and more.

---
+++ main.js
@@ -0,0 +1,22 @@
+// SIDEBAR TOOGLE
+
+var sidebarOpen = fales;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar(){
+    if(!sidebarOpen){
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen = true;
+    }
+}
+
+
+function closeSidebar(){
+    if(!sidebarOpen){
+        sidebar.classList.remove("sidebar-responsive");
+        sidebarOpen = false;
+    }
+}
+
+
+  
